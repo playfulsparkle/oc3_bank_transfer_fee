e47ef3e47d655096f7ce0bdc79446ef76dcf71b0
zsoltoroszlany82 <zsolt.oroszlany@playfulsparkle.com>
2026-02-16 17:49:38 +0100

    Bumped to version 1.0.3, add dummy install/uninstall

--- src/upload/admin/controller/extension/total/ps_bank_transfer_fee.php
+++ src/upload/admin/controller/extension/total/ps_bank_transfer_fee.php
@@ -105,4 +105,14 @@ class ControllerExtensionTotalPsBankTransferFee extends Controller
 
         return !$this->error;
     }
+
+    public function install()
+    {
+
+    }
+
+    public function uninstall()
+    {
+
+    }
 }
